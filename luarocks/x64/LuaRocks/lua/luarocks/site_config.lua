b69a2a4e90bc68976115b013cf3cd9b279bfa1e6
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-10-20 16:31:51 +0300

    Fix. `site_config` arch for x64

--- luarocks/x64/LuaRocks/lua/luarocks/site_config.lua
+++ luarocks/x64/LuaRocks/lua/luarocks/site_config.lua
@@ -1,7 +1,7 @@
 local site_config = {}
 site_config.LUAROCKS_UNAME_S=[[WindowsNT]]
-site_config.LUAROCKS_UNAME_M=[[x86]]
-site_config.LUAROCKS_PREFIX=[[c:\luarocks\x86\LuaRocks]]
+site_config.LUAROCKS_UNAME_M=[[x86_64]]
+site_config.LUAROCKS_PREFIX=[[c:\luarocks\x64\LuaRocks]]
 site_config.LUAROCKS_DOWNLOADER=[[wget]]
 site_config.LUAROCKS_MD5CHECKER=[[md5sum]]
 return site_config
